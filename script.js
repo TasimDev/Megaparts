57658fa1fc6a9c5f7047c310455bdd0c537b4f08
Tasim <118922118+TasimDev@users.noreply.github.com>
2023-07-19 00:31:22 +0300

    tasimchoo

---
+++ script.js
@@ -0,0 +1,16 @@
+const hamburgerBtn = document.querySelector("#hamburger-menu-btn");
+const mobileNav = document.querySelector("#mobile .container")
+
+hamburgerBtn.addEventListener("click", () => {
+    mobileNav.classList.toggle("active");
+    hamburgerBtn.classList.toggle("active");
+    if (!hamburgerBtn.classList.contains('active')) {
+        hamburgerBtn.innerHTML = `<span class="icon">
+        <svg xmlns="http://www.w3.org/2000/svg" width="27" height="18" viewBox="0 0 27 18" fill="none">
+            <path d="M1 1H26M1 9H26M1 17H26" stroke="white" stroke-width="2" stroke-linecap="round" />
+        </svg>
+    </span>`
+    } else {
+        hamburgerBtn.innerHTML = `<span class="icon text-white text-2xl font-bold cursor-pointer"> &#10005;</span>`
+    }
+})
